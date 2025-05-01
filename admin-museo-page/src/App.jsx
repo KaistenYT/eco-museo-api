eed060b3d7cddab0edc8abe0765ba8a7f5d5a379
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-05-01 10:41:56 -0400

    adminpage

---
+++ admin-museo-page/src/App.jsx
@@ -0,0 +1,15 @@
+import { BarraNav } from "./components/templates/BarraNav";
+import  Footer    from "./components/templates/Footer";
+  function App() {
+
+  return (
+    <>
+      <BarraNav />
+
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ admin-museo-page/src/components/templates/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="container-fluid">
+      <footer className="footer">
+        <p className="text-center">Admin Museo</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
