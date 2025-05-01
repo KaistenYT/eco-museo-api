39252722779aec54d417b23d2beb09871e7111f4
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-05-01 12:11:47 -0400

    cambios

--- admin-museo-page/src/App.jsx
+++ admin-museo-page/src/App.jsx
@@ -1,18 +1,35 @@
-import { BarraNav } from "./components/templates/BarraNav";
-import  Footer    from "./components/templates/Footer";
-import ActorTable from "./components/Actor/ActorTable";
-import HomePage from "./components/pages/HomePage";
-  function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Components
+import { BarraNav } from './components/templates/BarraNav';
+import Footer from './components/templates/Footer';
+import ActorTable from './components/Actor/ActorTable';
+import HomePage from './components/pages/HomePage';
+
+function App() {
   return (
-    <>
-      <BarraNav />
+    <Router>
+      <div className="app-container">
+        <BarraNav />
+        
+        <main className="main-content">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<HomePage />} 
+            />
+            <Route 
+              path="/actors" 
+              element={<ActorTable />} 
+            />
+            {/* Agrega más rutas aquí */}
+          </Routes>
+        </main>
 
-     <HomePage />
-
-      <Footer />
-    </>
-  )
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- admin-museo-page/src/components/templates/BarraNav.jsx
+++ admin-museo-page/src/components/templates/BarraNav.jsx
@@ -7,23 +7,23 @@ export function BarraNav() {
  <div>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Admin Museo</a>
+            <a className="navbar-brand" href="/">Admin Museo</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Actores</a>
+                        <a className="nav-link" href="/actors">Actores</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Autores</a>
+                        <a className="nav-link" href="/authors">Autores</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Historias</a>
+                        <a className="nav-link" href="/histories">Historias</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Participaciones</a>
+                        <a className="nav-link" href="/participations">Participaciones</a>
                     </li>
                 </ul>
             </div>
