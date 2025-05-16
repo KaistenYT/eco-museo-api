3584bc80af2c3be69c000d21e9c9cd721c6263f5
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-05-16 18:42:50 -0400

    rutas del nav

--- admin-museo-page/src/components/templates/BarraNav.jsx
+++ admin-museo-page/src/components/templates/BarraNav.jsx
@@ -1,34 +1,29 @@
-
+import { Link } from 'react-router-dom'; // Import the Link component
 
 export function BarraNav() {
-
-
   return (
- <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">Admin Museo</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/actors/list">Actores</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/authors/list">Autores</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/histories/list">Historias</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/participations/list">Participaciones</a>
-                    </li>
-                </ul>
-            </div>
+          <Link to="/" className="navbar-brand">Admin Museo</Link> {/* Use Link for the home page too */}
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/actors/list" className="nav-link">Actores</Link> {/* Use Link here */}
+              </li>
+              <li className="nav-item">
+                <Link to="/authors/list" className="nav-link">Autores</Link> {/* Use Link here */}
+              </li>
+              <li className="nav-item">
+                <Link to="/histories/list" className="nav-link">Historias</Link> {/* Use Link here */}
+              </li>
+            </ul>
+          </div>
         </div>
-    </nav>
- </div>
+      </nav>
+    </div>
   );
 }
