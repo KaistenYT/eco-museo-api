72ca6629b972f67b396f45c362476c34bf8b5430
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-05-19 19:04:52 -0400

    finu

--- admin-museo-page/src/components/utils/ApiFun.jsx
+++ admin-museo-page/src/components/utils/ApiFun.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = 'https://historias-v2-api.vercel.app'
+const API_URL = 'https://historias-api-crud-v2.vercel.app'
 /*const API_URL = 'http://localhost:3000';*/
 
 // Actors API
