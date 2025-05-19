baa33190e03f35d54d05f16d6b423046c52696db
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-05-18 20:17:11 -0400

    call

--- admin-museo-page/src/components/utils/ApiFun.jsx
+++ admin-museo-page/src/components/utils/ApiFun.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = 'https://historias-v2-api.vercel.app'
+const API_URL = 'https://historias-v2-api-git-cambios-para-la-45d328-kaistenyts-projects.vercel.app'
 /*const API_URL = 'http://localhost:3000';*/
 
 // Actors API
